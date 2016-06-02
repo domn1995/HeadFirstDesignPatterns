da9190a17ad78070e921a91754d24394db63f4e8
Domn Werner <domn.werner@gmail.com>
2016-06-01 20:21:07 -0700

    Added RemoteControlTest class.

---
+++ TestDrives/Command/RemoteControlTest/RemoteControlTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SimpleRemote;
+
+namespace RemoteControlTest
+{
+    public static class RemoteControlTest
+    {
+        public static void Main()
+        {
+            SimpleRemoteControl remote = new SimpleRemoteControl();
+            Light light = new Light();
+            GarageDoor garageDoor = new GarageDoor();
+            LightOnCommand lightOn = new LightOnCommand(light);
+            GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(garageDoor);
+
+            remote.Command = lightOn;
+            remote.ButtonWasPressed();
+            remote.Command = garageOpen;
+            remote.ButtonWasPressed();
+
+            Console.Read();
+        }
+    }
+}
